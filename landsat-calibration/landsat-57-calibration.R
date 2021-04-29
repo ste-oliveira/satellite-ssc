f4a553d57ea1b68bb6cf01c17399b986d6f0a064
Ste <stef.oliveira@yahoo.com.br>
2021-04-29 20:28:32 -0300

    modificacoes depois da reuniao

--- landsat-calibration/landsat-57-calibration.R
+++ landsat-calibration/landsat-57-calibration.R
@@ -214,7 +214,7 @@ library(PBSmapping)
                                    B7 = B7_median,
                                    nd52 = nd_median,
                                    num_pix = B2_count,
-                                   sample_dt = ymd(date),
+                                   Data = ymd(date),
                                    landsat_dt = ymd(date)
   )], cols = c('B1','B2','B3','B4','B5','B7'))[
     B1 > 0 & B2 > 0 & B3 > 0 & B4 > 0 & B5 > 0 & B7 > 0][
@@ -242,7 +242,7 @@ library(PBSmapping)
         station_nm = site_no,
         sensor = ifelse(grepl('LT',`system:index`),'Landsat 5','Landsat 7'))][ 
           # select only columns of interest
-          ,.(station_nm, sensor, site_no, Latitude,Longitude,sample_dt, num_pix, landsat_dt,
+          ,.(station_nm, sensor, site_no, Latitude,Longitude,Data, num_pix, landsat_dt,
              B1,B2,B3,B4,B5,B6,B7,B2.B1,B2.B1,B3.B1,B4.B1,B5.B1,B7.B1,B3.B2,B4.B2,B5.B2,
                B7.B2,B4.B3,B5.B3,B7.B3,B5.B4,B7.B4,B7.B5,nd52,cloud_cover,cloud_qa_count,snow_ice_qa_count, 
              solar_az, solar_zen,sr_atmos_opacity_median,sr_cloud_qa_median
@@ -299,41 +299,114 @@ library(PBSmapping)
                         , .(EstacaoCodigo = as.character(EstacaoCodigo), 
                             river_nm = paste0("River Name", " at ", "Fluviometric Station"), 
                             drainage_area_km2 = as.numeric("Drainage Area (KmÂ² )")*1000, 
-                            Latitude, Longitude)][EstacaoCodigo %chin% taquari_insitu_site_nos]
+                            Latitude, Longitude, sample_dt = date(ymd_hms(Data)))][EstacaoCodigo %chin% taquari_insitu_site_nos]
    
    setkey(taquari_insitu_raw,EstacaoCodigo)
    setkey(taquari_sts,EstacaoCodigo)
    
    # Join raw in situ data with site info, add and rename columns
-   taquari_insitu_raw_1 <- taquari_insitu_raw[taquari_sts][,':='(agency_cd = 'ANA',
-                                                               EstacaoCodigo = as.character(EstacaoCodigo),                                                  
-                                                     sample_depth_m = 'Surface', 
-                                                     sample_datetime = ymd_hms(Date),
-                                                     sample_dt = date(ymd_hms(Date)),
-                                                     # sample_tm = as.character(hms(ymd_hms(Date))),
-                                                     Q_cms = NA,
-                                                     data_type = 'qw',
-                                                     POC_mgL = NA,
-                                                     p63 = NA,
-                                                     width_m = NA,
-                                                     alt_m = NA,
-                                                     begin_date = min(date(ymd_hms(Date))),
-                                                     end_date = max(date(ymd_hms(Date)))
-   )][!is.na(SSC_mgL),.(agency_cd,station_nm, site_no, sample_dt, 
-                        SSC_mgL, POC_mgL, p63, sample_method, sampler, sample_depth_m, width_m,
-                        Latitude, Longitude, drainage_area_km2, alt_m,
-                        data_type,begin_date,end_date)]
-   
-  
+   #taquari_insitu_raw_1 <- taquari_insitu_raw[taquari_sts][,':='(agency_cd = 'ANA',
+   #                                                          EstacaoCodigo = as.character(EstacaoCodigo),                                                  
+   #                                                 sample_depth_m = 'Surface', 
+   #                                                 sample_datetime = ymd_hms(Date),
+   #                                                 sample_dt = date(ymd_hms(Date)),
+   #                                                 # sample_tm = as.character(hms(ymd_hms(Date))),
+   #                                                 Q_cms = NA,
+   #                                                 data_type = 'qw',
+   #                                                 POC_mgL = NA,
+   #                                                 p63 = NA,
+   #                                                 width_m = NA,
+   #                                                 alt_m = NA,
+   #                                                 begin_date = min(date(ymd_hms(Date))),
+   #                                                 end_date = max(date(ymd_hms(Date)))
+   #)][!is.na(SSC_mgL),.(agency_cd,station_nm, site_no, Data, 
+   #                    SSC_mgL, POC_mgL, p63, sample_method, sampler, sample_depth_m, width_m,
+   #                    Latitude, Longitude, drainage_area_km2, alt_m,
+   #                    data_type,begin_date,end_date)]
    
    
    
+   # Import depth integration calculations - Importar dados de profundidade da ANA 
+   #hybam_depth_integration <- fread('amazon-depth-integration.csv')
+   #setkey(hybam_depth_integration,station_nm)
+   #setkey(sa_insitu_raw_1,station_nm)
+  #Adjust surface measurements to depth integrated for deep SA rivers
+  #Add Depth integrated, calculated as the sample method and sample depth
+   #sa_insitu_raw_2 <- merge(sa_insitu_raw_1,hybam_depth_integration, 
+    #                         all = T, by = 'station_nm')[!is.na(Slope),
+    #                                                   ':='(SSC_mgL = SSC_mgL * Slope + Intercept,
+    #                                                          sample_method = 'Depth integrated, calculated',
+    #                                                          sample_depth_m = 'Depth integrated, calculated')]
+    
+   
+ #### --- ####
+ #### FINAL DATA PREPARATION: REMOVE SITES WITH INSUFFICIENT/UNSUITABLE DATA #### vou pular essa parte por enquanto
+   
+   ## Bind together in situ data from different agencies
+   all_acy_insitu_raw <- rbind(taquari_insitu_raw)[
+     is.na(sample_method), sample_method  := 'Unknown'][,
+                                                        match_name := ifelse(agency_cd %chin% c('ANA'),
+                                                                             station_nm,site_no)]
+    
+    #Select usable (>> drainage area threshold, no canyons) sites with sufficient Landsat images
+   all_acy_sts <- unique(all_acy_insitu_raw[match_name %chin% site_no_n100,match_name])
+   site_no_ls_insitu_n100 <- 
+     # Remove in situ data at stations with insufficient landsat images
+     all_acy_insitu_raw[match_name %chin% all_acy_sts]
+   
+   ## Remove Landsat data from unsuitable sites (insufficient drainage, canyons)
+   ls_clean <- ls_raw_1[site_no %chin% all_acy_sts]
+   
+ #### JOIN LANDSAT AND IN SITU DATA, WITH LAG OF UP TO 10 DAYS, RESTRICT TO < 3 DAYS ####
+ ## Join Landsat data with in situ data, allowing for as much as a 10-day lead/lag
+   
+     # Join Landsat data with in situ data
+   lag_days <- 8
+   # taquari_insitu_raw <- setDT(ls_clean)[all_acy_insitu_daily_mean, roll = lag_days][ # uni-directional join
+   taquari_insitu_raw_clean <- setDT(taquari_insitu_raw)[, ':='(match_dt_start = Data - lag_days,
+                                           match_dt_end = Data + lag_days,
+                                           Data = NULL)][
+                                             all_acy_insitu_daily_mean[,':='(match_dt = Data)], 
+                                             on = .(match_name == match_name, match_dt_start <= match_dt, match_dt_end >= match_dt)][ # bi-directional join
+                                               !is.na(B1)][ # removes days with in situ SSC but no Landsat image
+                                                 ,lag_days := as.numeric(difftime(Data,landsat_dt),'days')][ # calculate the lead/lag between in situ and satellite sample
+                                                   # Remove columns that are duplicated 
+                                                   ,':='(site_no = i.site_no, i.site_no = NULL, i.Latitude = NULL, i.Longitude = NULL, i.station_nm = NULL)][
+                                                     # Add Log10 SSC, squared cols, and remove no values, too cold (B6 < 269) values
+                                                     SSC_mgL > 0,':='(log10_SSC_mgL = log10(SSC_mgL),
+                                                                      B1.2 = B1^2,
+                                                                      B2.2 = B1^2,
+                                                                      B3.2 = B1^2,
+                                                                      B4.2 = B1^2,
+                                                                      B5.2 = B1^2,
+                                                                      B7.2 = B1^2
+                                                     )][!is.na(log10_SSC_mgL) & B6 > 268] 
    
    
+   # Select minimum lead/lag row
+   setkey(taquari_insitu_raw[,abs_lag_days := abs(lag_days)], abs_lag_days)
+   taquari_insitu_raw <- taquari_insitu_raw[, .SD[1], .(site_no, Data, data_type, sample_method, sampler)]
+   
+   fwrite(taquari_insitu_raw,paste0(wd_exports,'ls_insitu_match.csv'))
    
    
+   #### REGRESSION VARIABLES ####
+   # Select explanatory variables (regressors) for mulitple regression model
+   regressors_no_site <- c('B1', 'B2', 'B3', 'B4', 'B5', 'B7', # raw bands
+                           'B2.B1', 'B3.B1', 'B4.B1', 'B5.B1', 'B7.B1', # band ratios
+                           'B3.B2', 'B4.B2', 'B5.B2', 'B7.B2',
+                           'B4.B3', 'B5.B3', 'B7.B3',
+                           'B5.B4', 'B7.B4', 'B7.B5')
+   regressors_all <- c('B1', 'B2', 'B3', 'B4', 'B5', 'B7', # raw bands
+                       'B1.2', 'B2.2', 'B3.2', 'B4.2', 'B5.2', 'B7.2', # squared bands
+                       'site_no', # no clear way to add categorical variables
+                       'B2.B1', 'B3.B1', 'B4.B1', 'B5.B1', 'B7.B1', # band ratios
+                       'B3.B2', 'B4.B2', 'B5.B2', 'B7.B2',
+                       'B4.B3', 'B5.B3', 'B7.B3',
+                       'B5.B4', 'B7.B4', 'B7.B5')
    
-   
+   # Select regressors - bands and band ratios
+   regressors_primary <- c(regressors_no_site, 'B4.B3.B1') # all regressors
    
    
    
